a679a7f8cfc9ce9722fb77919d5afa1ad06a9aa9
wangxvyang <wangxuyang@iqusong.com>
2018-04-27 17:23:19 +0800

    第一次提交

---
+++ homejs.js
@@ -0,0 +1,31 @@
+
+/* 侧边栏切换形态 */
+$(".navbar-side a").click(function(){
+    $("body").toggleClass("sidebar-collapse");
+    if($("body").hasClass("sidebar-collapse")){
+        $(".sidebar > h4").html("11111111");
+    }else{
+        $(".sidebar > h4").html("22222222");
+    }
+    return false;
+})
+
+$(".sidenav>li>a").click(function(){
+    $(this).addClass("hover");
+    $(this).next().slideToggle();
+    $(this).parent().siblings().children("a").removeClass("hover").next().slideUp();
+
+})
+
+// 切换侧边栏
+$(function(){
+    // 切换侧边栏
+    $(".navbar-left li").click(toggleSidebar);
+});
+
+
+//切换侧边栏显示隐藏
+function toggleSidebar(){
+    $("body").toggleClass("big-page");
+    return false;
+}
